878cb1d1bf4eff6a627d7a6dade026c54fc22179
JATLY <131124699+Jatly@users.noreply.github.com>
2023-12-27 23:15:22 +0530

    Update dino.js

--- dino.js
+++ dino.js
@@ -1,4 +1,5 @@
 
+
 //board
 let board;
 let boardWidth = 800;
@@ -181,4 +182,3 @@ function detectCollision(a, b) {
            a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
            a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
 }
-
